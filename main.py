03305606acbf73c5dd58437dccc1f794b2941edb
Alena.Vozmishcheva <alena.vozmishcheva@gmail.com>
2024-11-26 22:37:49 +0300

    При помощи функции os.walk() выведено содержимое папки проекта.

--- main.py
+++ main.py
@@ -3,3 +3,8 @@ import os
 relative_path = "data_path_1/test_file_1.txt"
 absolute_path = os.path.abspath(relative_path)
 print(absolute_path)
+
+for folders in os.walk("."):
+    print(f"Папки: {folders}")
+for files in os.walk("."):
+    print(f"Файлы: {files}")
